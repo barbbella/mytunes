8fec9ee7ff8c733c95373f90f62be4e01ab38007
Kate Jefferson <barbedbella@gmail.com>
2015-02-18 17:40:19 -0600

    Setup completed. Queue presented on Index file.

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,23 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+  // ALL CODE IS KATE'S
+
+  tagName: 'tr',    // row tag
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'), // same as libraryentryview
+
+  // CODE BELOW FROM LIBRARYENTRYVIEW
+  //events: {
+  //  'click': function() {
+  //    this.model.play();
+  //  }
+  //},
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));  // same as libraryentryview
+  }
+
+
+
+
 });
